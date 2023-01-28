b478f3ee7354e606dc7bcda55ad7067a808741a4
fuyvivsxc <95850463+fuyvivsxc@users.noreply.github.com>
2023-01-28 16:55:35 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://yourdomain'
+const BAREURL = 'https://hide.zmep.tk/'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
